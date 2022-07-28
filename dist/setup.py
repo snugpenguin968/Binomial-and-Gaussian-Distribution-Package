e6782cca0fe835f95d234674d3804941b3983562
John Tian <johntian2014@gmail.com>
2022-07-28 16:47:04 -0500

    Delete setup.py

--- dist/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='binomial_gaussian_distribution_pkg',
-      version='1.0',
-      description='Binomial and Gaussian Distribution',
-      packages=['binomial_gaussian_distribution_pkg'],
-      author="John Tian",
-      author_email="johntian2014@gmail.com",
-      zip_safe=False)
